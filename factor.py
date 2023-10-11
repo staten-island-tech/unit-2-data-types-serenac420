fcd75977039a74a27fb34dc1d0db3a53f1529bfc
serenac420 <SErenac35@nycstudents.net>
2023-10-11 13:58:16 -0400

    m

--- factor.py
+++ factor.py
@@ -1,10 +1,13 @@
-
-def factors(x): 
-    for i in range: (1, x + 1)
+x = int(input('number: '))
+for i in range (1, x + 1):
     if x % i == 0: 
-       print(i)
-        
-x = 82
+        print(i)
+
+
+
+
+
+
 
 
 
